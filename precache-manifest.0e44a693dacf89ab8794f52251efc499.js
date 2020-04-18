22a868cc7cd1ef7ef018b79c3be405ba8aa46364
JeGarcia24 <jeralyn.garcia@yahoo.com>
2020-04-18 16:15:40 +0800

    Updates

---
+++ precache-manifest.0e44a693dacf89ab8794f52251efc499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf15558b2ee1ea49d3278c837693fb81",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "ce048d6500895a96526a",
+    "url": "/drum-machine/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "f345da871e06065bcbae",
+    "url": "/drum-machine/static/css/main.92f0fd69.chunk.css"
+  },
+  {
+    "revision": "ce048d6500895a96526a",
+    "url": "/drum-machine/static/js/2.f9e55b8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.f9e55b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f345da871e06065bcbae",
+    "url": "/drum-machine/static/js/main.4b1b46fb.chunk.js"
+  },
+  {
+    "revision": "ac7d9540cfdb54ee17c9",
+    "url": "/drum-machine/static/js/runtime-main.3d96963e.js"
+  }
+]);
